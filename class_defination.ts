d26bc59dbc8852137263c977e62877bbd8267fcc
Bhuvan Arora <er.bhuvanarora@gmail.com>
2019-05-05 13:13:24 +0530

    Creating class

---
+++ class_defination.ts
@@ -0,0 +1,15 @@
+class Tree
+{
+    //-------------- Way 1--------------------
+    // branch : string;
+    // constructor(branch)
+    // {
+    //     this.branch = branch;
+    // }
+
+    //----------------- way 2--------------
+    constructor(public branch:string)
+    {
+        this.branch = branch;
+    }
+}
